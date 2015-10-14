4e540ea0840cd190cd8dee71ff2b63544fe21697
ndunhourowens <ndunhour@hotmail.com>
2015-10-14 13:40:53 -1000

    test code for writing a file

--- server.js
+++ server.js
@@ -9,21 +9,28 @@ var fs = require('fs');
 // Create a server
 var server = http.createServer(function(request,response){
   var dataBuffer = "";
-  console.log(request);
+
+// **************** GET request
+
+// write a condition to verify a get request
 
 
   request.on('data', function(dataInFiles){
     dataBuffer += dataInFiles;
     // var data = qs.parse( dataBuffer.toString() );
   });
+console.log(request);
 
   request.on('end',function(){
     // read the input from the browser
     var inputFromBrowser = url.parse(request.url);
+    console.log('what this qs? \n', qs.parse);
 
-    fs.readFile('./public' + inputFromBrowser.path, function(err, dataInFiles){
+    // verify if the GET url request is in the public folder ==> return file
+    fs.readFile('./public/' + inputFromBrowser.path, function(err, dataInFiles){
         // return the 404.html
         if(err) {
+          // if url does not exist in public folder throw an error
           fs.readFile('./public/404.html',function(err2,dataFrom404) {
             response.end(dataFrom404.toString());
           });
@@ -31,21 +38,27 @@ var server = http.createServer(function(request,response){
           response.end(dataInFiles.toString());
         }
     });
-
-
-    // if user request for information it is a GET method...
-
-    // verify if the GET url request is in the public folder ==> return file
-
-    // if url does not exist in public folder throw an error
-
-
   });
 });
 
-
-
 // *************  POST request
+var eleName = 'eleName';
+var eleSymbol = 'eleSymbol';
+var eleAtomNum = 'eleAtomNum';
+var eleDes = 'eleDes';
+
+// var querystring = url.parse( request.url, true);
+
+var element = (
+  '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <title>The Elements - ' + eleName + '</title> <link rel="stylesheet" href="/css/styles.css"> </head> <body> <h1>' + eleName + '</h1> <h2>' + eleSymbol + '</h2> <h3>' + eleAtomNum +'</h3> <p>' + eleDes + '</p> <p><a href="/">back</a></p> </body> </html>'
+);
+
+
+fs.writeFile( './public/' + eleName + '.html', element, function(err){
+  if(err) throw new Error('could not write to testing.html' + err.message);
+   console.log('done write to uber.txt');
+
+});
 
 server.listen(PORT, function(){
 
