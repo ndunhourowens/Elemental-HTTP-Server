1a7c01eed349a400e0c911745cae759949691aaa
ndunhourowens <ndunhour@hotmail.com>
2015-10-13 15:46:52 -1000

    add code to styles.css, helium, hydrogen, index, sever, 404. html

---
+++ server.js
@@ -0,0 +1,13 @@
+// require/import the HTTP module
+var http = require('http');
+var PORT = 3000;
+var url = require('url');
+var qs = require('querystring');
+// Create a server
+var server = http.createServer(function(request,response){
+
+});
+
+// requestListener is a function that is added to the 'request' event
+  // event: 'request'
+
