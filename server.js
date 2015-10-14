95335bd3546f152997a3a52db9e0b6214c9e35b7
ndunhourowens <ndunhour@hotmail.com>
2015-10-13 19:44:56 -1000

    partially got GET request to work.  Need to use GET

--- server.js
+++ server.js
@@ -3,20 +3,42 @@ var http = require('http');
 var PORT = 3000;
 var url = require('url');
 var qs = require('querystring');
+
+// reading files
+var fs = require('fs');
 // Create a server
 var server = http.createServer(function(request,response){
-    console.log('do something in data');
   var dataBuffer = "";
 
-  request.on('data', function(data){
-    console.log(dataBuffer);
-    dataBuffer += data;
+  request.on('data', function(dataInFiles){
+    dataBuffer += dataInFiles;
+    // var data = qs.parse( dataBuffer.toString() );
   });
 
   request.on('end',function(){
-    // info from the request POST body
-    var data = qs.parse( dataBuffer.toString() );
-    response.end( JSON.stringify(data) );
+    // read the input from the browser
+    var inputFromBrowser = url.parse(request.url);
+    console.log(inputFromBrowser);
+
+
+    fs.readFile('./public' + inputFromBrowser.path, function(err, dataInFiles){
+        // return the 404.html
+        if(err) {
+          fs.readFile('./public/404.html',function(err2,dataFrom404) {
+            response.end(dataFrom404.toString());
+          });
+        } else {
+          response.end(dataInFiles.toString());
+        }
+    });
+
+
+    // if user request for information it is a GET method...
+
+    // verify if the GET url request is in the public folder ==> return file
+
+    // if url does not exist in public folder throw an error
+
 
   });
 });
