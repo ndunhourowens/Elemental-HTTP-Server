48c02df875d7cd4a53f844cb003fe4ef6603f268
ndunhourowens <ndunhour@hotmail.com>
2015-10-13 16:24:28 -1000

    got server to work

--- server.js
+++ server.js
@@ -5,9 +5,26 @@ var url = require('url');
 var qs = require('querystring');
 // Create a server
 var server = http.createServer(function(request,response){
+    console.log('do something in data');
+  var dataBuffer = "";
 
+  request.on('data', function(data){
+    console.log(dataBuffer);
+    dataBuffer += data;
+  });
+
+  request.on('end',function(){
+    // info from the request POST body
+    var data = qs.parse( dataBuffer.toString() );
+    response.end( JSON.stringify(data) );
+
+  });
 });
 
-// requestListener is a function that is added to the 'request' event
-  // event: 'request'
 
+
+// *************  POST request
+
+server.listen(PORT, function(){
+
+});
